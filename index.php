877659e4810f16262bc029e13f7a499c820cf279
ArturSantos-dev <78228369+ArturSantos-dev@users.noreply.github.com>
2021-02-09 11:53:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1>Itaú turma 14b</h1>
 <h1>Ola alunos</h1>
 <h2>Teste Pradox</h2>
+<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0177fc1c-bbe9-4012-b726-f5fdd95e5b16/d210mzk-12a0d390-4331-42a9-bc81-ea0a02f850c7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMDE3N2ZjMWMtYmJlOS00MDEyLWI3MjYtZjVmZGQ5NWU1YjE2XC9kMjEwbXprLTEyYTBkMzkwLTQzMzEtNDJhOS1iYzgxLWVhMGEwMmY4NTBjNy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.CZWcW5TJrxxitmbofO8QTasn0Rd7xfSLYS4RzVBh8zU">
 <h2> Guten Morgan</h2>
 <h1>Ola 1234567</h1>
